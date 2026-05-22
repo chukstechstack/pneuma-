e2bec0900e20dad533a50484cd01953af26b17bb
Chukwunyelu Kingsley Emmanuel <chuks.techstack@gmail.com>
2026-05-22 20:55:28 +0100

    Refactor layout styles, resolve mobile shaking loops, and implement crisp white button design

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -39,7 +39,7 @@ const HomePage = () => {
     return <FullPageLoader />;
   }
 
-  const defaultProfileImg = "https://dev.to";
+  const defaultProfileImg = "https://media2.dev.to/dynamic/image/width=800%2Cheight=%2Cfit=scale-down%2Cgravity=auto%2Cformat=auto/https%3A%2F%2Fwww.gravatar.com%2Favatar%2F2c7d99fe281ecd3bcd65ab915bac6dd5%3Fs%3D250";
 
   return (
     <div className="home-layout">
